9d6b1172e3d2937c27ddb25d6ed167030ef3dc37
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2023-01-11 14:36:48 +0100

    Added correct project

---
+++ Services/final/westcoast-cars/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './ui/navigation/navigation.component';
+import { VehicleListComponent } from './vehicles/vehicle-list.component';
+import { VehicleDetailsComponent } from './vehicles/vehicle-details.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    VehicleListComponent,
+    VehicleDetailsComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
